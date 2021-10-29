45de5d2d7ad27bdde4e33630a10ab5d90a1a91d5
Maciej Mikulski <maciej.mikulski@sparklab.pl>
2021-10-29 21:05:21 +0200

    Basket constructor arguments.

--- src/Basket/Basket.php
+++ src/Basket/Basket.php
@@ -4,9 +4,31 @@ declare(strict_types=1);
 
 namespace Acme\Basket;
 
+use Acme\DeliveryRule;
+use Acme\ProductCatalogue;
+use Acme\ProductOffer;
+
 class Basket
 {
-    public function __construct()
+    private ProductCatalogue $catalogue;
+    /**
+     * @var array|DeliveryRule[]
+     */
+    private array $deliveryRules;
+    /**
+     * @var array|ProductOffer[]
+     */
+    private array $offers;
+
+    /**
+     * @param ProductCatalogue $catalogue
+     * @param array $deliveryRules|DeliveryRule[]
+     * @param array $offers|Offer[]
+     */
+    public function __construct(ProductCatalogue $catalogue, array $deliveryRules, array $offers)
     {
+        $this->catalogue = $catalogue;
+        $this->deliveryRules = $deliveryRules;
+        $this->offers = $offers;
     }
 }
--- tests/BasketTest.php
+++ tests/BasketTest.php
@@ -3,14 +3,21 @@
 declare(strict_types=1);
 
 use Acme\Basket\Basket;
+use Acme\DeliveryRule;
+use Acme\ProductCatalogue;
+use Acme\ProductOffer;
 use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertInstanceOf;
 
 class BasketTest extends TestCase
 {
-  final public function testClassInitialised(): void
+  final public function testBasketInitialisedWithCatalogueRulesAndOffers(): void
   {
-    $basket = new Basket();
-    assertInstanceOf(Basket::class, $basket);
+      $catalogue = new ProductCatalogue();
+      $deliveryRule = new DeliveryRule();
+      $offer = new ProductOffer();
+
+      $basket = new Basket($catalogue, [$deliveryRule], [$offer]);
+      assertInstanceOf(Basket::class, $basket);
   }
 }
